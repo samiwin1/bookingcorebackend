b273bd1876034e2ad9e8f055b999a118a741a354
sami <sami.bousnina2001@gmail.com>
2024-06-15 17:56:11 +0200

    eeeerrrrrr

--- booking-service/src/booking.schema.ts
+++ booking-service/src/booking.schema.ts
@@ -3,9 +3,9 @@ import { Document } from 'mongoose';
 
 @Schema()
 export class Booking extends Document {
-    @Prop()
-    id: string;
-
+    @Prop({ type: String, required: true })
+    _id: string;
+    
     @Prop({ required: true })
     name: string;
 
